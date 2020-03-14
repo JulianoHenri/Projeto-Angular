4990a4f5b726a5e2b23ef3667b86e2e07906e06e
Juliano <jubarzrhd@gmail.com>
2020-03-14 19:43:29 -0300

    Criado Category Form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit,AfterContentChecked } from '@angular/core';
-import{FormBuilder,FormControl,FormGroup,Validators} from '@angular/forms'
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from "@angular/router"
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
+import { switchMap } from 'rxjs/operators';
+
+import toastr from "toastr";
 
 @Component({
   selector: 'app-category-form',
@@ -8,13 +14,119 @@ import{FormBuilder,FormControl,FormGroup,Validators} from '@angular/forms'
 })
 export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMenssages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
-  ngAfterContentChecked(){
+  submitForm(){
+    this.submittingForm = true;
 
+    if(this.currentAction == 'new'){
+      this.createCategory();
+    }else{
+      this.updateCategory();
+    }
   }
 
+  ngAfterContentChecked() {
+    this.setPageTitle()
+
+  }
+  //Metodos Privados
+
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path == "new") {
+      this.currentAction = "new"
+    } else
+      this.currentAction = "edit"
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    })
+  }
+
+  private loadCategory() {
+    if (this.currentAction == "edit") {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get("id")))
+      )
+        .subscribe(
+          (category) => {
+            this.category = category
+            this.categoryForm.patchValue(category)// bind loaded category data to CategoryForm.
+          },
+          (error) => alert('Ocorreu um Erro No Servidor, Tente mais tarde.')
+        )
+    }
+  }
+
+  private setPageTitle() {
+    if (this.currentAction == 'new') 
+      this.pageTitle = 'Cadastro de Nova Categoria'
+    else {
+      const categoryName = this.category.name || ""
+      this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory(){
+    const category: Category = Object.assign (new Category(), this.categoryForm.value);
+    this.categoryService.create(category)
+      .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+    
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign (new Category(), this.categoryForm.value);
+
+    this.categoryService.update(category)
+    .subscribe(
+      category => this.actionsForSuccess(category),
+      error => this.actionsForError(error)
+    )
+  }
+
+  actionsForError(error){
+    toastr.error('Ocorreu eum erro ao processar a sua socilicitação');
+
+    this.submittingForm = false
+
+    if(error.status === 422){
+      this.serverErrorMenssages = JSON.parse(error._body).errors;
+    }else{
+      this.serverErrorMenssages = ["Falha na comunicação com o servidor tente novamente mais tarde"]
+    }
+  }
+  
+  actionsForSuccess(category: Category) {
+    toastr.success("Solicitação processada com Sucesso!");
+    //redirect/reolad component page
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () =>this.router.navigate(["categories", category.id, "edit"])
+    )
+  }
+  
 }
